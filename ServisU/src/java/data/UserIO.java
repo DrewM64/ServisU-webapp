aef1a6fb105942af0b3cd613456bc4e2d85f504c
daniel <daniel@daniel-VirtualBox>
2022-03-18 23:31:31 -0400

    Add LoginServlet.java Modified UserIO.java logic to read file and return User object

--- ServisU/src/java/data/UserIO.java
+++ ServisU/src/java/data/UserIO.java
@@ -5,10 +5,50 @@
  */
 package data;
 
+import java.io.*;
+import java.util.*;
+import models.User;
+
 /**
  *
  * @author daniel
  */
 public class UserIO {
+    
+    //Returns User object based on username in specified file
+    public static User getUser(String filepath, String uNameInput) {
+        //read file
+        //if usernames match, return User object
+        //else, return null
+        try {
+            File file = new File(filepath);
+            BufferedReader in = new BufferedReader(
+                                new FileReader(file));
+
+            String line = in.readLine();
+            while (line != null) {
+//                Each line in file is read, data separated by comma
+                StringTokenizer t = new StringTokenizer(line, ",");
+                String uName = t.nextToken();
+                
+//              check if username on file matches username from input
+                if (uName.equalsIgnoreCase(uNameInput)) {
+                    String pWord = t.nextToken();
+                    User user = new User();
+                    user.setUsername(uName);
+                    user.setPassword(pWord);
+                    in.close();
+                    return user;
+                }
+//                Repeat for next line until end of file
+                line = in.readLine();
+            }
+            in.close();
+            return null;
+        } catch (IOException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
     
 }
